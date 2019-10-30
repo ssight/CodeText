c2cc4fc754f3b552001827836ef7b58cabad9460
ssight <{57152291}+{ssight}@users.noreply.github.com`>
2019-10-30 19:01:29 +0000

    Created the website

---
+++ script.js
@@ -0,0 +1,68 @@
+document.addEventListener('DOMContentLoaded', () => {
+    getColor();
+})
+
+function getColor() {
+    document.getElementById("color-text").innerHTML = "Currently " + document.getElementById("backgroundColor").value;
+    document.getElementById("color-text-foreground").innerHTML = "Currently " + document.getElementById("foregroundColor").value;
+}
+
+function checkFont() {
+    if (document.getElementById('fontLocation').value.replace(/ /g, "") !== "") {
+        if (new Detector().detect(document.getElementById('fontLocation').value)) {
+            document.getElementById("fontCheck").innerHTML = "It's installed! 🎉";
+            return true;
+        } else {
+            document.getElementById("fontCheck").innerHTML = "You don't seem to have that.";
+            return false;
+        }
+    } else {
+        document.getElementById("fontCheck").innerHTML = "Please enter a name first.";
+        return false;
+    }
+}
+
+var styles = {
+    "font-family": "",
+    "background": "",
+    "color": "",
+    "border-radius": "",
+    "padding": ""
+}
+
+var cssGen = "";
+
+function createStyle() {
+    if (checkFont()) {
+        styles = {
+            "font-family": `${document.getElementById('fontLocation').value}!important`,
+            "background": `${document.getElementById("backgroundColor").value}!important`,
+            "color": `${document.getElementById("foregroundColor").value}!important`,
+            "border-radius": `${document.getElementById("borderRadius").value}px!important`,
+            "font-size": `${document.getElementById("fontSize").value}px!important`
+        }
+
+        cssGen = "code,pre,.blob-code,.blob-code>*{";
+        Object.entries(styles).forEach(i => {
+            cssGen += `${i[0]}:${i[1]};`;
+        }); cssGen += "}pre{padding:" + document.getElementById("padding").value + "px!important}" +
+            `code{padding:${document.getElementById("padding2").value}px}pre>code{padding:0}`;
+
+        cssGen = cssGen.replace(/pxpx/g, "px");
+
+        return true
+    } else {
+        document.getElementById("preview").innerHTML = "You might want to check that font.<br>If you're not sure, just type `monospace`.";
+        return false;
+    }
+}
+
+function preview() {
+    if (createStyle()) {
+        let s = document.createElement('style');
+        s.innerHTML = cssGen;
+        document.head.appendChild(s);
+        document.getElementById("preview").innerHTML = "Success! 😀 Hope you like it.";
+        document.getElementById("create").style.display = "block";
+    }
+}
